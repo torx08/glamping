3e5d302cb8742ca321f151a806db8b04ba35ed86
torx <tor.vesterled@gmail.com>
2025-11-05 11:42:58 +0100

    Pageheader and styling
    Closes 1#

---
+++ src/assets/components/PageFooter.jsx
@@ -0,0 +1,9 @@
+const PageFooter = () => {
+  return (
+    <footer>
+      <h2>titel</h2>
+    </footer>
+  );
+};
+
+export default PageFooter;
---
+++ src/assets/components/PageHeader.jsx
@@ -0,0 +1,10 @@
+import styles from './pageHeader.module.css';
+const PageHeader = () => {
+  return (
+    <header className={styles.header}>
+      <h1>titel</h1>
+    </header>
+  );
+};
+
+export default PageHeader;
